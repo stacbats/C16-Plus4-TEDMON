fd05406974fd16ed4afb184856994957fb0aa7de
Stacy Bates (stacbats) <44994840+stacbats@users.noreply.github.com>
2022-01-22 09:54:38 +0000

    Add files via upload

--- page41.asm
+++ page41.asm
@@ -6,12 +6,15 @@ BasicUpstart2(main)
 
 main:
 
-    lda #$00
-    sta $0400       // plus4 $0c00
+    lda #$00        // Load 0 into accumulator (# immediate)
+    sta $0400       // Store it at $0400plus4 $0c00
+    sta $d800       // ALso putting 0 into address $d800
 L1: lda $0400       // plus4 $0c00
+    lda $d800
     clc
-    adc #$04
+    adc #$09
     sta $0400       // plus4 $0c00
+    sta $d800
     jmp L1
 
     rts
